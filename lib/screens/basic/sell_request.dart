c50b33ad3958c857b756091194de3cabda5e4771
Reeya31 <reeyabudhachhetri@gmail.com>
2024-01-02 11:56:58 +0545

    12

--- lib/screens/basic/sell_request.dart
+++ lib/screens/basic/sell_request.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart' as geo;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class SellRequest extends StatefulWidget {
   const SellRequest({Key? key}) : super(key: key);
@@ -13,29 +14,24 @@ class SellRequest extends StatefulWidget {
 }
 
 class _SellRequestState extends State<SellRequest> {
+  geo.Position? currentPositionOfUser;
+  final TextEditingController addressController = TextEditingController();
+
   late GoogleMapController googleMapController;
-  late Marker userMarker = Marker(markerId: const MarkerId('currentLocation')); // Declare userMarker variable
+  late Marker userMarker = Marker(markerId: const MarkerId('currentLocation'));
   Set<Marker> markers = {};
 
   @override
   void initState() {
     super.initState();
     userMarker = Marker(markerId: const MarkerId('currentLocation'));
-     _getCurrentLocation();
+    getCurrentLocation();
   }
 
-  String _selectedWasteType = 'Plastic';
+  // String _selectedWasteType = 'Plastic';
   String _selectedWasteQuantity = 'Below 1kg';
 
-  List<String> _wasteTypes = [
-    'Plastic',
-    'Paper',
-    'Glass',
-    'Metal & Steel',
-    'E-Waste',
-  ];
-
-List<String> wasteType = ["Plastic", "Paper", "Glass", "Metal & Steel", "E-Waste"];
+  List<String> wasteType = ["Plastic", "Paper", "Glass", "e-Waste"];
   List<bool> selectedWaste = [false, false, false, false];
 
   List<String> _wasteQuantities = [
@@ -44,63 +40,75 @@ List<String> wasteType = ["Plastic", "Paper", "Glass", "Metal & Steel", "E-Waste
     'Above 5 kg',
   ];
 
+  Future<void> getCurrentLocationOfUser() async {
+    geo.Position positionOfUser = await geo.Geolocator.getCurrentPosition(
+        desiredAccuracy: geo.LocationAccuracy.best);
+
+    currentPositionOfUser = positionOfUser;
+
+    LatLng(currentPositionOfUser!.latitude, currentPositionOfUser!.longitude);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: Stack(
-          children: [
-            GoogleMap(
-        initialCameraPosition: CameraPosition(target: LatLng(27.672468, 85.337924), zoom: 14),
-        markers: markers,
-        zoomControlsEnabled: false,
-        mapType: MapType.normal,
-        onMapCreated: (GoogleMapController controller) {
-          googleMapController = controller;
-        },
-        onTap: (LatLng latLng) {
-          _addOrUpdateMarker(latLng);
-        },
-      ),
-
-      
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                padding: EdgeInsets.all(16),
-                color: Colors.white,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: SlidingUpPanel(
+          minHeight: 100,
+          maxHeight: MediaQuery.of(context).size.height - 200,
+          panel: Container(
+            padding: EdgeInsets.all(16),
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  controller: addressController,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    suffixIcon: const Icon(Icons.location_on),
+                  ),
+                ),
+                Column(
                   children: [
-                  Row(
-                      children: [
-                        Text('Waste Type :', style: TextStyle(fontSize: 18),),
-                        SizedBox(width: 12,),
-                        DropdownButton<String>(
-                      value: _selectedWasteType,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _selectedWasteType = newValue!;
-                        });
-                      },
-                      items: _wasteTypes.map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
+                    Text(
+                      'Waste Type :',
+                      style: TextStyle(fontSize: 18),
                     ),
-                      ],
+                    SizedBox(
+                      width: 12,
                     ),
-                    
-                    SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Text('Waste Quantity:',style: TextStyle(fontSize: 18),),
-                        SizedBox(width: 12,),
+                     ListView.builder(
+              shrinkWrap: true,
+              itemCount: wasteType.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                    title: Text(wasteType[index]),
+                    value: selectedWaste[index],
+                    onChanged: (value) {
+                      setState(() {
+                        selectedWaste[index] = value!;
+                      });
+                    });
+              },
+            ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Waste Quantity:',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
                     DropdownButton<String>(
                       value: _selectedWasteQuantity,
                       onChanged: (String? newValue) {
@@ -115,55 +123,69 @@ List<String> wasteType = ["Plastic", "Paper", "Glass", "Metal & Steel", "E-Waste
                         );
                       }).toList(),
                     ),
-                      ],
-                    ),
-                    
-                    SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle send pickup request here
-                        // Access selected waste type: _selectedWasteType
-                        // Access selected waste quantity: _selectedW
-                      },
-                      child: Text('Send Pickup Request', style: TextStyle(fontSize: 15,color: Colors.white),),
-                    ),
                   ],
                 ),
-              ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle send pickup request here
+                    // Access selected waste type: _selectedWasteType
+                    // Access selected waste quantity: _selectedWasteQuantity
+                  },
+                  child: Text(
+                    'Send Pickup Request',
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
+          body: GoogleMap(
+            initialCameraPosition: const CameraPosition(
+                target: LatLng(27.672468, 85.337924), zoom: 14),
+            markers: markers,
+            zoomControlsEnabled: false,
+            mapType: MapType.normal,
+            onMapCreated: (GoogleMapController controller) {
+              googleMapController = controller;
+            },
+            onTap: (LatLng latLng) {
+              addOrUpdateMarker(latLng);
+            },
+          ),
         ),
       ),
     );
   }
-  Future<void> _getCurrentLocation() async {
-  try {
-    Position position = await _determinePosition();
 
-    setState(() {
-      userMarker = Marker(
-        markerId: const MarkerId('currentLocation'),
-        position: LatLng(position.latitude, position.longitude),
-        draggable: true,
-        onDragEnd: (newPosition) {
-          _getPlaceName(newPosition);
-        },
-      );
-      markers.clear();
-      markers.add(userMarker);
-    });
-  } catch (e) {
-    print("Error fetching location: $e");
-  }
-}
-
-
-  Future<void> _getPlaceName(LatLng position) async {
+  Future<void> getCurrentLocation() async {
     try {
-      List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+      geo.Position position = await determinePosition();
+
+      setState(() {
+        userMarker = Marker(
+          markerId: const MarkerId('currentLocation'),
+          position: LatLng(position.latitude, position.longitude),
+          draggable: true,
+          onDragEnd: (newPosition) {
+            getPlaceName(newPosition);
+          },
+        );
+        markers.clear();
+        markers.add(userMarker);
+      });
+    } catch (e) {
+      print("Error fetching location: $e");
+    }
+  }
+
+  Future<void> getPlaceName(LatLng position) async {
+    try {
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
       if (placemarks.isNotEmpty) {
         Placemark place = placemarks.first;
-        print("Place Name: ${place.name}"); // Display place name in console
+        print("Place Name: ${place.name}");
         // You can show place name in UI or handle it as required
       }
     } catch (e) {
@@ -171,7 +193,7 @@ List<String> wasteType = ["Plastic", "Paper", "Glass", "Metal & Steel", "E-Waste
     }
   }
 
-  void _addOrUpdateMarker(LatLng latLng) {
+  void addOrUpdateMarker(LatLng latLng) {
     setState(() {
       markers.remove(userMarker);
       userMarker = Marker(
@@ -179,40 +201,39 @@ List<String> wasteType = ["Plastic", "Paper", "Glass", "Metal & Steel", "E-Waste
         position: latLng,
         draggable: true,
         onDragEnd: (newPosition) {
-          _getPlaceName(newPosition);
+          getPlaceName(newPosition);
         },
       );
       markers.add(userMarker);
     });
   }
 
-  Future<Position> _determinePosition() async {
+  Future<geo.Position> determinePosition() async {
     bool serviceEnabled;
-    LocationPermission permission;
+    geo.LocationPermission permission;
 
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    serviceEnabled = await geo.Geolocator.isLocationServiceEnabled();
 
     if (!serviceEnabled) {
       throw 'Location services are disabled';
     }
 
-    permission = await Geolocator.checkPermission();
+    permission = await geo.Geolocator.checkPermission();
 
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
+    if (permission == geo.LocationPermission.denied) {
+      permission = await geo.Geolocator.requestPermission();
 
-      if (permission == LocationPermission.denied) {
+      if (permission == geo.LocationPermission.denied) {
         throw 'Location permission denied';
       }
     }
 
-    if (permission == LocationPermission.deniedForever) {
+    if (permission == geo.LocationPermission.deniedForever) {
       throw 'Location permissions are permanently denied';
     }
 
-    Position position = await Geolocator.getCurrentPosition();
+    geo.Position position = await geo.Geolocator.getCurrentPosition();
 
     return position;
   }
-
 }
